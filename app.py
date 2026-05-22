7e634d93547ec17dfdfcd03c52acbbd7de8798ee
taseenparveen2 <taseenparveen43@gmail.com>
2026-05-22 08:27:54 +0530

    fixed deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.document_loaders import (
     Docx2txtLoader
 )
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
 from langchain_classic.chains.retrieval_qa.base import RetrievalQA
